050af6f8f9271234130d87c4f1c6247286b8b7e7
SoyLuna1107 <soyluna0904@gmail.com>
2021-12-29 17:19:44 -0500

    seventh Commit

--- src/public/js/crearPerfil.js
+++ src/public/js/crearPerfil.js
@@ -2,9 +2,6 @@ document.addEventListener("DOMContentLoaded", (e) => {
     const id = document.getElementById("username").textContent;
     console.log(id);
   
-    postData("/nomina/getUsername", {id}).then((res) => {
-    //   console.log(res.PER_CNIVEL);
-  
-    });
+    
   });
   
--- src/routes/nominaP.js
+++ src/routes/nominaP.js
@@ -7,21 +7,52 @@ const Class2 = require("../Class2");
 const { DeCrypt } = require("../public/js/Class2");
 const { query } = require("../database");
 
-let permiso='';
- 
+let permiso = "";
+let permiso1 = "ADMINISTRADOR";
+let permiso2 = "GERENTE";
+let permiso3 = "JEFE DE OPERACION";
+
+var sqlTipoPermiso = "";
+//FUNCION PARA ESTABLECER LA CONSULTA DE CARGOS DEPENDIENDO DEL PERMISO
+function crearConsultaPermisosCaros(nivelPermiso) {
+  if (nivelPermiso == permiso1) {
+    sqlTipoPermiso = "SELECT EST_CDETALLE FROM dbp_nomina.TBL_RESTANDAR WHERE EST_CCONSULTA ='cmbCargo'";
+  } else if (nivelPermiso == permiso2) {
+    sqlTipoPermiso = "SELECT EST_CDETALLE FROM dbp_nomina.TBL_RESTANDAR WHERE EST_CCONSULTA ='cmbCargo' AND EST_CDETALLE3='GERENTE' OR EST_CDETALLE3='JEFE DE OPERACION' ORDER BY EST_CDETALLE ASC";
+  } else if (nivelPermiso == permiso3) {
+    sqlTipoPermiso = "SELECT EST_CDETALLE FROM dbp_nomina.TBL_RESTANDAR WHERE EST_CCONSULTA ='cmbCargo' AND EST_CDETALLE3='JEFE DE OPERACION' ORDER BY EST_CDETALLE ASC";
+  }
+
+  return sqlTipoPermiso;
+}
 
 router.get("/crearPerfil/:username", (req, res) => {
-   let username = req.params.username;
-   const sqlPermiso = "SELECT PER_CNIVEL FROM dbp_nomina.TBL_RPERMISO WHERE FKPER_CUSUARIOS=?";
-   db.promise()
-  .query(sqlPermiso,[username])
-  .then(([resultPermiso]) => {
-    console.log(resultPermiso);
-    // res.render("nomina/nomListCampanas", { title: "Nomina Campañas", campañas:resultCampañas});
-  })
-  .catch((err) => console.log("ERROR::", err));
-  
-  });
+  let username = req.params.username;
+  const sqlUserPermiso = "SELECT * FROM dbp_nomina.TBL_RPERMISO WHERE FKPER_CUSUARIOS=?";
+  db.promise()
+    .query(sqlUserPermiso, [username])
+    .then(([resultPermiso]) => {
+      //cosultar cargos
+      var consultaTipoPerCargo = "";
+      consultaTipoPerCargo = crearConsultaPermisosCaros(resultPermiso[0].PER_CNIVEL);
+      db.promise()
+        .query(consultaTipoPerCargo)
+        .then(([resultTipoPermiso]) => {
+            //consultar campañas
+          const sqlCampañas = "SELECT EST_CDETALLE FROM dbp_nomina.TBL_RESTANDAR WHERE EST_CDETALLE2=? OR EST_CDETALLE2=?";
+        //   console.log("Id", resultPermiso[0].PKPER_NCODIGO);
+          db.promise()
+            .query(sqlCampañas,[resultPermiso[0].PKPER_NCODIGO,resultPermiso[0].PKPER_NCODIGO])
+            .then(([resultCampanias]) => {
+                console.log(resultCampanias);
+              res.render("nomina/crearPerfil", { title: "Crear Perfil", dataCargos: resultTipoPermiso,dataCampanas:resultCampanias });
+            })
+            .catch((err) => console.log("ERROR::", err));
+        })
+        .catch((err) => console.log("ERROR::", err));
+    })
+    .catch((err) => console.log("ERROR::", err));
+});
 
 //obtener usuario para comparar permisos de select
 
